ad86866733bdc94a8f171572e2644000c989bfb4
Siti Anasuha <anasuharosli@gmail.com>
2024-06-30 13:57:53 +0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,9 @@
 <?php 
 
 
-$link = mysqli_connect("localhost","jrmfamil_ans","Anasuh@97!","jrmfamil_jrmns");
+$link = mysqli_connect("employee-management-system.c7icmygw6c2g.ap-southeast-1.rds.amazonaws.com","admin","4r5oMIZzEHzMaOvcZTCj","employee-management-system");
 //$link = mysqli_connect("localhost", "root", "", "jrmns");
+//
  
 // Check connection
 if($link === false){
